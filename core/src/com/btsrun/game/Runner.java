9b369b9e4615ef781467dc43e341d3ee458208e0
fabed2976 <fabed2976@HRH-IC0027343.wrdsb.ca>
2018-01-16 09:51:33 -0500

    Added some comments in runner class

--- core/src/com/btsrun/game/Runner.java
+++ core/src/com/btsrun/game/Runner.java
@@ -1,12 +1,12 @@
 package com.btsrun.game;
 
 
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 
 
@@ -16,60 +16,121 @@ import com.badlogic.gdx.utils.Array;
  */
 public class Runner {
     
-   // private float gravity;
-   // private final float Maximum_DY = 13.0f;
-    private Vector3 pos;
-    private Vector3 vel;
-    private Rectangle bounds;
-    private final float MOVE = 100;
-    private float statetime = 0;
+    //character location
+    private float x;
+    private float y;
     
-    //Animation for the Runner to run
-    private Animation runnerun;
+    //chracter animation
+    private float dx;
+    private float dy;
     
-    public Runner(int x, int y){
-        pos = new Vector3(x,y,0);
-        vel = new Vector3 (MOVE, 0, 0);
-       // this.gravity = 0.9f;
+    //gravity for the player
+    private float gravity;
+    
+    //the maximum of gravity that the runner can jump
+    private final float Max_DY = 13.0f;
+    
+    //the time of //time of program running
+    private float elapsed;
+    
+    //animation variables to allow player movement
+    private Animation<TextureRegion> run;
+    private Animation<TextureRegion> runR;
+    
+    //pictures of standing character
+    private TextureRegion stand;
+    
+    //create texture atlas to help load images of character
+    private TextureAtlas atlas;
+
+    /**
+     * character constructor
+     *
+     * @param x
+     * @param y
+     */
+    public Runner(float x, float y) {
+        //set starting point
+        this.x = x;
+        this.y = y;
+
+        //character standing position
+        this.dx = 0;
+        this.dy = 0;
+
+        //intialize the gravity
+        this.gravity = 0.9f;
         
-        Array<TextureRegion> frames = new Array<TextureRegion>();
-        for(int  i = 1; i <= 6; i++){
-            frames.add(new TextureRegion(new Texture("Chracter" + i + ".png")));
-            
+        //no animation at starting point
+        this.elapsed = 0;
+
+        //load images into texture atlas
+        this.atlas = new TextureAtlas("packed/runner.atlas");
+
+        //find standing image and load to program
+        this.stand = atlas.findRegion("character2");
+
+        //create a run animation by finding each character image
+        run = new Animation(1f / 10f, atlas.findRegions("character"));
+
+        //load character images again but this time enables them to be flipped through
+        Array<TextureRegion> runRFrames = new Array<TextureRegion>();
+        for (int i = 1; i <= 6; i++) {
+            //goes through each image
+            runRFrames.add(atlas.findRegion("character" + i));
         }
-        runnerun = new Animation(0.2f , frames);
-        runnerun.setPlayMode(Animation.PlayMode.LOOP);
-        bounds = new Rectangle(pos.x , pos.y, runnerun.getKeyFrames()[0].getTexture().getWidth() );
-       
-        
+
+        //create the forward animation
+        runR = new Animation(1f / 10f, runRFrames);
     }
-    
-    public void update(float deltaTime){
-        vel.scl(deltaTime);
-        pos.add(vel);
-        vel.scl(1 / deltaTime);
-        
-        bounds.setPosition(pos.x, pos.y);
-        statetime += deltaTime;
+/**
+ * Getting the X position of the runner
+ * @return the x value
+ */
+    public float getX() {
+        return x;
     }
-    public void render(SpriteBatch batch){
-        batch.draw(runnerun.getKeyFrame(statetime), pos.x, pos.y);
-        
-    }
-    
-    public float getX(){
-        return pos.x;
-    }
-    public float getY(){
-        return pos.y;
-    }
-    
-    public Rectangle getBounds(){
-        return bounds;
-    }
-    
-    public void dispose(){
-        //runner.dispose();
+/**
+ * Getting the Y position of the runner
+ * @return the y value
+ */
+    public float getY() {
+        return y;
     }
 
+    /**
+     *updates program by adding the program time to the time 
+     * @param deltaTime
+     */
+    public void update(float deltaTime) {
+        this.elapsed = this.elapsed + deltaTime;
+        
+        //if you pressed the keys up button
+        if(Gdx.input.isKeyJustPressed(Input.Keys.UP)){
+            this.dy = 15;
+        }
+        
+        //gravity
+        if(dy > -Max_DY ){
+            this.dy -= gravity;
+        }
+
+    }
+
+    /**
+     *displays character during the program as animation
+     * @param batch
+     */
+    public void render(SpriteBatch batch) {
+        batch.draw(runR.getKeyFrame(elapsed, true), x, y, 90,90);
+
+    }
+
+    // get rid of heavy objects
+    public void dispose() {
+        atlas.dispose();
+    }
 }
+    
+   
+
