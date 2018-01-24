856e325c4d231ba9a67a4a9e6e1b8b41d6d86615
DhanaAnvic <fabed2976@googleapps.wrdsb.ca>
2018-01-24 13:14:33 -0500

    Update Background.java

--- core/src/com/btsrun/game/Background.java
+++ core/src/com/btsrun/game/Background.java
@@ -17,10 +17,12 @@ public class Background {
 
     //variable for the background
     Texture background;
+    Texture background2;
     
     //variable for the X and Y value of the background
     private float bgX1, bgY2;
-    
+    private  float bgX2, bgY2;
+
     //variable for the animation
     private float elapsed;
     
@@ -30,11 +32,15 @@ public class Background {
     public Background() {
         //loading the image for the background
         background = new Texture(Gdx.files.internal("seoul.png"));
+        background2 = new Texture(Gdx.files.internal("seoul.png"));
         
         //the x and y axis coordinate for the background
         bgX1 = 0;
         bgY2 = 0;
         
+         //the x and y axis coordinate for the 2nd background
+        bgX2 = 800;
+        bgY2 = 0;
 
         //no animation at starting point
         this.elapsed = 0;
@@ -47,32 +53,50 @@ public class Background {
     public float getBGX1() {
         return bgX1;
     }
+    
+/**
+ *Getting the X position of the 2nd background
+ * @ return the x value
+ */
+    public float getBGX2() {
+        return bgX2;
+    }
+    
 /**
  * Drawing our background image
  * @param batch 
  */
     public void render(SpriteBatch batch) {
         batch.draw(background, bgX1, bgY2);
-
-
+        batch.draw(background2, bgX2, bgY2);
     }
+    
 /**
  * This method is how the background will make the scrolling
  * @param deltaTime 
  */
     public void update(float deltaTime) {
+        //
         bgX1 -= deltaTime * speed;
-
+        bgX2 -= deltaTime * speed;
+        
+        //create an if statement for the background 
+        //if the X Axis of the backgeound and the background width is less than 0
+        // add the second background with the same width
         if (bgX1 + background.getWidth() < 0) {
             bgX1 = bgX1 + background.getWidth();
-            this.elapsed = bgX1 + this.elapsed;
             
         }
-        
+        if( bgX2 + background2.getWidth() < 0){
+            bgX2 = bgX1 + background.getWidth();
+   
+        }
+
     }
 
     public void dispose() {
         background.dispose();
+        background2.dispose();
 
 
     }
